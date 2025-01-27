9896e1ab9141af6be2dbeb3569367ae0e693ab07
Pedro Mendonça <althander@gmail.com>
2025-01-27 10:46:56 -0300

    Adição dos métodos de deleção em cascata para a comunicação assincrona

--- course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -51,6 +51,8 @@ public class CourseServiceImpl implements CourseService {
             moduleRepository.deleteAll(moduleModelList);
         }
 
+        courseRepository.deleteCourseUserByCourse(courseModel.getCourseId());
+
         courseRepository.delete(courseModel);
 
     }
