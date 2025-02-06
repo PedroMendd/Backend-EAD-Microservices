763168241b7bedfa429eb4610ad3c439bd54c9f7
Pedro Mendonça <althander@gmail.com>
2025-02-06 10:17:32 -0300

    Criação de configs e consumer para Topic Exchange

--- notification/src/main/java/com/ead/notification/services/Impl/NotificationServiceImpl.java
+++ notification/src/main/java/com/ead/notification/services/Impl/NotificationServiceImpl.java
@@ -1,8 +1,15 @@
 package com.ead.notification.services.Impl;
 
+import com.ead.notification.dtos.NotificationRecordCommandDto;
+import com.ead.notification.enums.NotificationStatus;
+import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
@@ -12,4 +19,18 @@ public class NotificationServiceImpl implements NotificationService {
     public NotificationServiceImpl(NotificationRepository notificationRepository) {
         this.notificationRepository = notificationRepository;
     }
+
+    @Override
+    public NotificationModel saveNotification(NotificationRecordCommandDto notificationRecordCommandDto) {
+
+        var notificationModel = new NotificationModel();
+
+        BeanUtils.copyProperties(notificationRecordCommandDto, notificationModel);
+        notificationModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        notificationModel.setNotificationStatus(NotificationStatus.CREATED);
+
+        return notificationRepository.save(notificationModel);
+
+    }
+
 }
