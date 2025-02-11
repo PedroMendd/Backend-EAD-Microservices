3d5d6095051e4491d862c3838cea1920727d9f0a
Pedro Mendonça <althander@gmail.com>
2025-02-11 12:25:12 -0300

    Começando as configs de Security para MS AUTHUSER

---
+++ authuser/src/main/java/com/ead/authuser/models/RoleModel.java
@@ -0,0 +1,30 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "TB_ROLES")
+public class RoleModel implements Serializable, GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true, length = 30)
+    private RoleType roleName;
+
+    @Override
+    public String getAuthority() {
+        return this.roleName.toString();
+    }
+}
